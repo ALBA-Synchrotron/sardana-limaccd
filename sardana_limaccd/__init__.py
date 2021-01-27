16162e925fe69d98f0731bab0f248f6afbc3b5e2
Roberto Javier Homs Puron <rhoms@cells.es>
2021-01-27 12:29:49 +0100

    Add setup.py and bumpversion configuration

---
+++ sardana_limaccd/__init__.py
@@ -0,0 +1,3 @@
+# The version is updated automatically with bumpversion
+# Do not update manually
+__version__ = "0.0.0"
---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+# The version is updated automatically with bumpversion
+# Do not update manually
+__version = "0.0.0"
+
+
+setup(
+    name="sardana_limaccd",
+    version=__version,
+    author="ALBA controls team",
+    author_email="controls@cells.es",
+    description="LimaCCD Sardana plugin (controllers, macros, tools)",
+    license="GPLv3",
+    url="https://github.com/ALBA-Synchrotron/sardana-limaccd",
+    packages=find_packages(),
+    install_requires=["sardana"],
+    python_requires=">=3.5"
+)
